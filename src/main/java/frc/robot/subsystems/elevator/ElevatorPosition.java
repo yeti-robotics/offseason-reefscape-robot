31a179bbecd5b806fe10501d68ccca46267f21d7
Ankit-Panda-2010 <ankitpanda2010@gmail.com>
2025-08-31 21:32:51 -0400

    ElevatorPosition Enum Set Up

--- src/main/java/frc/robot/subsystems/elevator/ElevatorPosition.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorPosition.java
@@ -1,4 +1,22 @@
 package frc.robot.subsystems.elevator;
 
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+
 public enum ElevatorPosition {
+    BOTTOM(0.0);
+
+    private final Angle height;
+
+    ElevatorPosition(double height) {
+        this(Units.Rotations.of(height));
+    }
+
+    ElevatorPosition(Angle height) {
+        this.height = height;
+    }
+
+    public Angle getHeight() {
+        return height;
+    }
 }

98324bf3de0916fb44f69497fc69e2fe08f00008
tkopp <tkoppuravuri@gmail.com>
2025-09-03 18:23:46 -0400

    Motion magic values

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConfig.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConfig.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 
 class ElevatorConfig {
     //Placeholder IDs for now
@@ -15,8 +16,15 @@ class ElevatorConfig {
 
     private static final Slot0Configs SLOT_0_CONFIGS =
             new Slot0Configs();
-    private static final Slot1Configs SLOT_1_CONFIGS =
-            new Slot1Configs();
+    private static final Slot1Configs SLOT_1_CONFIGS = new Slot1Configs()
+            .withKP(4)
+            .withKI(0)
+            .withKD(48)
+            .withKG(0)
+            .withKV(0)
+            .withKA(1)
+            .withKS(0.5)
+            .withGravityType(GravityTypeValue.Elevator_Static);
 
     static final TalonFXConfiguration primaryTalonFXConfigs =
             new TalonFXConfiguration();

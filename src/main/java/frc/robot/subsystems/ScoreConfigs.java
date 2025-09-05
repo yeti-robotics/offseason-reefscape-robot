9254ff45f6b03d3c34b6430655eed4069e68f932
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2025-09-04 20:59:13 -0400

    added speed var

--- src/main/java/frc/robot/subsystems/ScoreConfigs.java
+++ src/main/java/frc/robot/subsystems/ScoreConfigs.java
@@ -6,4 +6,5 @@ class ScoreConfigs {
     static final int scoringMotorID = 5;
     static final int canRangeStartID = 20;
     static final int canRangeEndID = 29;
+    static final double scoringMotorSpeed = 0.5;
 }

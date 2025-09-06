6244f2b95d3a6218868c351baa307cf8b1cd075a
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2025-09-05 22:37:01 -0400

    updated spin rollers

--- src/main/java/frc/robot/subsystems/ScoringSubsystem.java
+++ src/main/java/frc/robot/subsystems/ScoringSubsystem.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.constants.Constants.motorCANBus;
 import static frc.robot.subsystems.ScoreConfigs.scoringMotorSpeed;
 
 public class ScoringSubsystem extends SubsystemBase {
@@ -12,22 +13,18 @@ public class ScoringSubsystem extends SubsystemBase {
     private final CANrange outsideCANRange;
 
     public ScoringSubsystem() {
-        scoringMotor = new TalonFX(ScoreConfigs.scoringMotorID);
-        insideCANRange = new CANrange(ScoreConfigs.canRangeStartID);
-        outsideCANRange = new CANrange(ScoreConfigs.canRangeEndID);
-    }
-
-    public boolean coralInMechanism() {
-        return (insideCANRange.getIsDetected().getValue() && outsideCANRange.getIsDetected().getValue());
+        scoringMotor = new TalonFX(ScoreConfigs.scoringMotorID, motorCANBus);
+        insideCANRange = new CANrange(ScoreConfigs.canRangeStartID, motorCANBus);
+        outsideCANRange = new CANrange(ScoreConfigs.canRangeEndID, motorCANBus);
     }
 
     public boolean coralIsSafe() {
-        return (outsideCANRange.getIsDetected().getValue() && !insideCANRange.getIsDetected().getValue());
+        return outsideCANRange.getIsDetected().getValue() && !insideCANRange.getIsDetected().getValue();
     }
 
-    private void setScoringSpeed(double scoringMotorSpeed) { scoringMotor.set(scoringMotorSpeed); }
+    private void setScoringSpeed(double scoringMotorSpeed) {scoringMotor.set(scoringMotorSpeed); }
 
     private void stopMotor() {scoringMotor.stopMotor();}
 
-    public Command spinRollers(double scoringMotorSpeed) {return startEnd(() -> setScoringSpeed(coralInMechanism() ? scoringMotorSpeed : 0), this::stopMotor); }
+    public Command spinRollers(double scoringMotorSpeed) {return runEnd(() -> setScoringSpeed(coralIsSafe() ? 0 : scoringMotorSpeed), this::stopMotor);}
 }

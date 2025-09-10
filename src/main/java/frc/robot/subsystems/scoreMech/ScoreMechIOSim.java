4aa6c90cfde16e90299bd5fdabfbfd95b11754b0
Prathik Saduneni <151557515+prthik@users.noreply.github.com>
2025-09-10 19:55:11 -0400

    added physics sim for motor

--- src/main/java/frc/robot/subsystems/scoreMech/ScoreMechIOSim.java
+++ src/main/java/frc/robot/subsystems/scoreMech/ScoreMechIOSim.java
@@ -1,4 +1,27 @@
 package frc.robot.subsystems.scoreMech;
 
-public class ScoreMechIOSim {
+import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.util.sim.PhysicsSim;
+
+import static frc.robot.constants.Constants.motorCANBus;
+
+public class ScoreMechIOSim implements ScoreMechIO{
+    private TalonFX scoreMotor;
+    private CANrange innerCANrange;
+    private CANrange outerCANrange;
+
+    public void ScoreMechIOSim(){
+        scoreMotor = new TalonFX(ScoreConfigs.scoreMotorID, motorCANBus);
+        innerCANrange = new CANrange(ScoreConfigs.innerCANrangeID, motorCANBus);
+        outerCANrange = new CANrange(ScoreConfigs.outerCANrangeID, motorCANBus);
+        PhysicsSim.getInstance().addTalonFX(scoreMotor);
+    }
+
+
+    public void updateInputs(ScoreMechIOInputs inputs) {
+        inputs.scoreVelocity = scoreMotor.getVelocity().getValueAsDouble();
+        inputs.innerSensorDetected = innerCANrange.getIsDetected().getValue();
+        inputs.outerSensorDetected = outerCANrange.getIsDetected().getValue();
+    }
 }

3e54fcdf0f5e8e68fe6cfd346905ea3b9dba3abd
yetirobotics <yetiroboticsengineering@gmail.com>
2025-10-11 14:21:47 -0400

    Update at 'Sat Oct 11 14:21:46 EDT 2025'

--- src/main/java/frc/robot/commands/ReefAlignPPOTF.java
+++ src/main/java/frc/robot/commands/ReefAlignPPOTF.java
@@ -35,7 +35,7 @@ public class ReefAlignPPOTF {
     private final SwerveRequest.Idle stopReq = new SwerveRequest.Idle();
 
     private static final Transform2d leftBranchTransform = new Transform2d(
-            Units.inchesToMeters(8),
+            Units.inchesToMeters(6),
             Units.inchesToMeters(-3.5),
             Rotation2d.k180deg); // negative x gets you closer, positive is further
     private static final Transform2d rightBranchTransform = new Transform2d(
